3136b52d3370eeda0ee0d5a8c5120c19834a0887
enercif <eniscif@googlemail.com>
2026-09-15 23:54:15 +0200

    Initial Commit

---
+++ paylas/paylasApp.swift
@@ -0,0 +1,17 @@
+//
+//  paylasApp.swift
+//  paylas
+//
+//  Created by Enis Erdem Ciftci on 15.09.26.
+//
+
+import SwiftUI
+
+@main
+struct paylasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ paylasTests/paylasTests.swift
@@ -0,0 +1,19 @@
+//
+//  paylasTests.swift
+//  paylasTests
+//
+//  Created by Enis Erdem Ciftci on 15.09.26.
+//
+
+import Testing
+@testable import paylas
+
+struct paylasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
